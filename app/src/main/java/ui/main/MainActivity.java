8e1fdd6d606a17857c6d7e9da3ad74ad3749c4dc
sulowski.dominik <sulowski.dominik@gmail.com>
2017-12-15 17:31:35 +0100

    Receiver

--- app/src/main/java/ui/main/MainActivity.java
+++ app/src/main/java/ui/main/MainActivity.java
@@ -3,10 +3,14 @@ package ui.main;
 import android.content.Context;
 import android.content.res.XmlResourceParser;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.marci.rss_projectjava.R;
@@ -18,6 +22,7 @@ import ui.overview.OverviewFragment;
 public class MainActivity extends AppCompatActivity implements OverviewFragmentActivityListener {
 
   private FragmentManager fragmentManager = getSupportFragmentManager();
+  private Button btnCheck;
   public DatabaseHelper myDb;
 
   @Override
@@ -34,6 +39,19 @@ public class MainActivity extends AppCompatActivity implements OverviewFragmentA
     //przykładowe dane - dla testu
     //myDb.insertByleCo();
       myDb.DeleteAllData();
+
+      btnCheck = (Button) findViewById(R.id.btn_check);
+
+      // Manually checking internet connection
+      checkConnection();
+
+      btnCheck.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              // Manually checking internet connection
+              checkConnection();
+          }
+      });
   }
 
   @Override
@@ -83,4 +101,29 @@ public class MainActivity extends AppCompatActivity implements OverviewFragmentA
         .commit();
   }
 
+    // Method to manually check connection status
+    private void checkConnection() {
+        boolean isConnected = ConnectivityReceiver.isConnected();
+        showSnack(isConnected);
+    }
+
+    // Showing the status in Snackbar
+    private void showSnack(boolean isConnected) {
+        String message;
+        int mColor;
+
+        if (isConnected) {
+            message = "Good! Connected to Internet";
+            mColor = Color.GREEN;
+        } else {
+            message = "Sorry! Not connected to internet";
+            mColor = Color.RED;
+        }
+
+        Toast toast =  Toast.makeText(this, message, Toast.LENGTH_LONG);
+
+        TextView toastMessage = (TextView) toast.getView().findViewById(android.R.id.message);
+        toastMessage.setTextColor(mColor);
+        toast.show();
+    }
 }
